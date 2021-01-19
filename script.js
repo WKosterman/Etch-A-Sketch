d2c25602f945457b4410a11c32f6ee1dfb53914d
Wessel Kosterman <wd@kosterman.eu>
2021-01-19 21:33:59 +0100

    Added a clear button

--- script.js
+++ script.js
@@ -3,18 +3,40 @@
 
 const gridContainer = document.querySelector('#grid');
 
+var amountOfSquares = 16;
 
+buildgrid()
 
-for (var i = 0; i < 4; i++) { 
-    let rowDiv = document.createElement('div');
-    rowDiv.id = i;
-    rowDiv.classList.add('rowDiv');
-    gridContainer.appendChild(rowDiv);
-    for (var j = 0; j < 4; j++) { 
-        let colSpan = document.createElement('span');
-        colSpan.id = i;
-        colSpan.classList.add('colSpan');
-        colSpan.textContent = "TestCol";
-        rowDiv.appendChild(rowDiv);
-    }
+const button = document.querySelector('#clearGrid');
+
+button.addEventListener('click', () => {
+    rebuildGrid()
+});
+
+function rebuildGrid() {
+    //amountOfSquares = prompt("How many squares?", 16);
+    console.log(amountOfSquares);
+    let colSpans = document.querySelectorAll('span');
+    [].forEach.call(colSpans, function(el) {
+        el.classList.remove("mouseInSpan");
+    });
 }
+
+function buildgrid() {
+
+    for (var i = 0; i < 4; i++) { 
+        let rowDiv = document.createElement('div');
+        rowDiv.id = i;
+        rowDiv.classList.add('rowDiv');
+        gridContainer.appendChild(rowDiv);
+        for (var j = 0; j < 4; j++) { 
+            let colSpan = document.createElement('span');
+            colSpan.id = i*10+j;
+            colSpan.classList.add('colSpan');
+            colSpan.addEventListener('mouseover', () => {
+                colSpan.classList.add('mouseInSpan');
+            });
+            rowDiv.appendChild(colSpan);
+        };
+    };
+};
