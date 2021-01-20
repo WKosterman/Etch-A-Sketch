e17bec7147f89860e440983fc37ec0f657a5697a
Wessel Kosterman <wd@kosterman.eu>
2021-01-20 22:48:13 +0100

    Made the size of the grid user input

--- script.js
+++ script.js
@@ -10,26 +10,36 @@ buildgrid()
 const button = document.querySelector('#clearGrid');
 
 button.addEventListener('click', () => {
-    rebuildGrid()
+    clearGrid()
+    buildgrid()
 });
 
-function rebuildGrid() {
-    //amountOfSquares = prompt("How many squares?", 16);
-    console.log(amountOfSquares);
+function clearGrid() {
+    let userìnput = prompt("How many columns?", 16)
+    amountOfSquares = Math.min(parseInt(userìnput),100);
     let colSpans = document.querySelectorAll('span');
     [].forEach.call(colSpans, function(el) {
         el.classList.remove("mouseInSpan");
+        el.parentNode.removeChild(el); 
+    });
+    let rowDivs = document.querySelectorAll(".rowDiv");
+    [].forEach.call(rowDivs, function(el) {
+        el.parentNode.removeChild(el); 
     });
 }
 
 function buildgrid() {
 
-    for (var i = 0; i < 4; i++) { 
+    let squareSize = (800 / amountOfSquares) + "px";
+    var r = document.querySelector(':root');
+    r.style.setProperty('--squareSize', squareSize);
+
+    for (let i = 0; i < amountOfSquares; i++) { 
         let rowDiv = document.createElement('div');
         rowDiv.id = i;
         rowDiv.classList.add('rowDiv');
         gridContainer.appendChild(rowDiv);
-        for (var j = 0; j < 4; j++) { 
+        for (let j = 0; j < amountOfSquares; j++) { 
             let colSpan = document.createElement('span');
             colSpan.id = i*10+j;
             colSpan.classList.add('colSpan');
